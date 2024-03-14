419509d711e2263a58135dc275fc52036bd0cb0a
Steven Burrell <sburrell23@gmail.com>
2024-03-14 15:50:09 -0600

    fixed config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/aigame/'
+  ? '/The_Everything_Map/'
   : '/'
 })
